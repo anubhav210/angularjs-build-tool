20fe2be04a074339aadbe8624190fc105979ab9d
Anubhav Rai <anuraics@gmail.com>
2019-07-17 15:09:06 +0530

    modified script which will take file from outside of project directory

--- gruntfile.js
+++ gruntfile.js
@@ -5,24 +5,39 @@ module.exports = function(grunt) {
             options: {
                 singleQuotes: true
             },
-            app: { //"app" target
-                files: {
-                    './public/min-safe/js/appFactory.js': ['./public/js/appFactory.js'],
-                    './public/min-safe/js/FormController.js': ['./public/js/FormController.js'],
-                    './public/min-safe/app.js': ['./public/js/app.js']
-                }
-            }
+            // app: { //"app" target
+            //     files: {
+            //         './public/min-safe/js/appFactory.js': ['./public/js/appFactory.js'],
+            //         './public/min-safe/js/FormController.js': ['./public/js/FormController.js'],
+            //         './public/min-safe/app.js': ['./public/js/app.js']
+                   
+
+            //     }
+            // },
+            all: {
+                files: [
+                    {
+                        expand: true,
+                        src: ['../compas/scripts/compas/**/*.js'],
+                        flatten: true,
+                        dest:"./annoted",
+                        ext: '.js',
+                        extDot: 'last'
+                        
+                    }
+                ],
+            },
         },
         concat: {
             js: { //target
-                src: ['./public/min-safe/app.js','./public/min-safe/js/*.js'],
-                dest: './public/min/app.js'
+                src: ['./annoted/*.js'],
+                dest: './dist/build.js',
             }
         },
         uglify: {
             js: { //target
-                src: ['./public/min/app.js'],
-                dest: './public/min/app.js'
+                src: ['./dist/build.js'],
+                dest: './dist/build.min.js'
             }
         }
 
@@ -32,7 +47,7 @@ module.exports = function(grunt) {
     });
     //load grunt task
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-uglify-es');
     grunt.loadNpmTasks('grunt-ng-annotate');
     //register grunt default task
     grunt.registerTask('default', ['ngAnnotate', 'concat', 'uglify']);
--- public/js/app.js
+++
@@ -1,4 +0,0 @@
-var app = angular.module('app', [
-    'app.factory',
-    'app.controller'
-    ]);
